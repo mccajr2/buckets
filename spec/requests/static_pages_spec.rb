e016b5122cace6dfc51d87ceb73315d27ef9ba6d
Jay McCarthy <mccarthy.jay@gmail.com>
2013-04-03 19:05:44 -0400

    Switch to postgresql

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Buckets" }
 
   describe "Home page" do
 
@@ -9,10 +10,10 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Buckets | Helping You Reach Your Goals')
     end
 
-    it "should have the title 'Buckets | Helping You Reach Your Goals'" do
+    it "should have the title 'Helping You Reach Your Goals'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Buckets | Helping You Reach Your Goals")
+                        :text => "#{base_title} | Helping You Reach Your Goals")
     end
   end
 
@@ -23,10 +24,10 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Getting the Most Out of Your Buckets')
     end
 
-    it "should have the title 'Buckets | Getting the Most Out of Your Buckets'" do
+    it "should have the title 'Getting the Most Out of Your Buckets'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Buckets | Getting the Most Out of Your Buckets")
+                        :text => "#{base_title} | Getting the Most Out of Your Buckets")
     end
   end
 
@@ -40,7 +41,22 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Buckets | About Us")
+                    :text => "#{base_title} | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+		    :text => "#{base_title} | Contact")
+    end
+  end
+
 end
