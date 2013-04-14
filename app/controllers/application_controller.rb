6e79d418fed54c1fd575ffa96bd78af4960496ec
Jason R. McCarthy <mccarthy.jay@gmail.com>
2013-04-14 10:54:07 -0400

    Fix logged in user root path issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def after_sign_in_path_for(user)
-    users_dashboard_path
+    root_path
   end
 
-  def signed_in_root_path(user)
-    user_url(user)
-  end
 end
